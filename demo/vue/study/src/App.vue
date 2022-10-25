47a4b47c7d7603f1566b25af84123fa858d57313
sadanya <2930048068@qq.com>
2022-10-25 12:02:31 +0800

    xue xi vue

--- demo/vue/study/src/App.vue
+++ demo/vue/study/src/App.vue
@@ -1,32 +1,15 @@
 <style>
-@import './assets/style.css';
 
-.app {
-  width: 100%;
-  height: 100%;
-  background: #fff;
-  display: flex;
-  flex-flow: column;
-  gap: 1em;
-}
 </style>
 
 <template>
-  <div class="app">
-    <UserSearch />
-    <UserList />
+  <div>
+
   </div>
 </template>
 
 <script>
-import UserSearch from './components/UserSearch'
-import UserList from './components/UserList'
-
 export default {
-  name: 'App',
-  components: {
-    UserSearch,
-    UserList
-  }
+  name: 'App'
 }
 </script>
--- demo/vue/study/src/main.js
+++ demo/vue/study/src/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
+
 import App from './App'
 
-Vue.config.productionTip = false
-
 new Vue({
-  render: h => h(App),
-  beforeCreate() {
-    Vue.prototype.$bus = this
-  }
+  render: h => h(App)
 }).$mount('#app')
